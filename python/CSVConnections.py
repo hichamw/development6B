288dab6c3b665d37d707592b952cbb075577cc41
sjaakslingerkaak <maartenf93@gmail.com>
2015-11-11 22:08:47 +0100

    Python bestanden aangepast

---
+++ python/CSVConnections.py
@@ -0,0 +1,53 @@
+# CSV Connections Handler for Giz
+
+import csv
+import MySQLdb
+
+#Read CSV file
+with open('../csv/Connections.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=';')
+    datetime = []
+    unitids = []
+    ports = []
+    values = []
+
+    #Loop and append to array
+    for row in readCSV:
+        datetime.append(row[0])
+        unitids.append(row[1])
+        ports.append(row[2])
+        values.append(row[3])
+
+    datetimecol = datetime[0]
+    unitidcol = unitids[0]
+    portcol = ports[0]
+    valuecol = values[0]
+
+    print(datetime)
+    print(unitids)
+    datetime.pop(0)
+    unitids.pop(0)
+    ports.pop(0)
+    values.pop(0)
+    print(datetime)
+    print(unitids)
+
+    print(datetime[0])
+
+    db = MySQLdb.connect("localhost","root","root","CSV")
+    cursor = db.cursor()
+    cursor.execute("SELECT VERSION()")
+    data = cursor.fetchone()
+    #print "Database version : %s " % data
+
+    createtable = "CREATE TABLE CONNECTIONS " + "("+ datetimecol + " DATETIME NOT NULL, " + unitidcol + " BIGINT(200), " + portcol + " VARCHAR(20), " + valuecol + " INT(10)) "
+    cursor.execute(createtable)
+
+    for i in xrange(1,len(datetime)):
+        insertall = "INSERT INTO CONNECTIONS " + "("+datetimecol+", "+unitidcol+", "+portcol+", "+valuecol+") VALUES (""'"+datetime[i]+"',"+"'"+unitids[i]+"',"+"'"+ports[i]+"',""'"+values[i]+"') "
+        #print(insertall)
+        cursor.execute(insertall)
+        db.commit()
+
+
+
