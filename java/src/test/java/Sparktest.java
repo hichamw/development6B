f8eba2639675d7f90132ffaaf1bfa8c6ac3e6a0e
hichamw <0897587@hr.nl>
2016-01-20 14:27:24 +0100

    testing TeamCity

--- java/src/test/java/Sparktest.java
+++ java/src/test/java/Sparktest.java
@@ -27,7 +27,7 @@ public class Sparktest {
             String query = "SELECT UnitID FROM CONNECTIONS";
             stmt.execute(query);
             ResultSet resultSet = stmt.getResultSet();
-
+            //testcomment
             while(resultSet.next()){
                 ids.add(resultSet.getBigDecimal("UnitID"));
             }
